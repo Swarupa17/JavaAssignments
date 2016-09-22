fb6be42d7481d1885f404b83f89a2b830bd28a2e
Swarupa17 <swarupa.khapli@yahoo.in>
2016-09-22 10:50:28 +0530

    Second session assignment

---
+++ Assignment2.1/src/PrintExample.java
@@ -0,0 +1,12 @@
+
+public class PrintExample {
+ public static void main(String args [])
+ {
+	 int i=0;
+	 System.out.println("Print Name Without Using Semicolon");
+	 for(i=1;i<2;System.out.println("Swarupa") )
+	 {
+		 i++;
+	 }
+ }
+}
