3e90a694b1735c0c5b6570e161e2169e44a90a4d
Swarupa17 <swarupa.khapli@yahoo.in>
2016-09-23 11:18:41 +0530

    String comparision

---
+++ Assignment3.2/src/StringExample.java
@@ -0,0 +1,11 @@
+
+public class StringExample {
+	public static void main(String args[])
+	{
+		String s1= "I am Student";
+		String s2="I am Student";
+		System.out.println("The Value of comparision between string s1 and s2 is "+s1.equals(s2));
+		
+	}
+
+}
