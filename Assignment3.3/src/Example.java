d9887534b722590e484182101b6dc4209d0dfbdb
Swarupa17 <swarupa.khapli@yahoo.in>
2016-09-23 11:21:07 +0530

    Test ouput

---
+++ Assignment3.3/src/Example.java
@@ -0,0 +1,12 @@
+
+public class Example {
+    int x; int y;
+    String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Example pnt = new Example(); 
+		   System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y); 
+	}
+
+}
+//pnt is null 0 0
