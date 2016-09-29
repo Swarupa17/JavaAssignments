1a9d2367028933a64c979fdf03604a9c2edf1c12
Swarupa17 <swarupa.khapli@yahoo.in>
2016-09-29 21:33:11 +0530

    Changes

--- Assignment4.4/src/ChildClass.java
+++ Assignment4.4/src/ChildClass.java
@@ -2,12 +2,12 @@
 public class ChildClass extends ParentClass {
 	public ChildClass()
 	{
-		System.out.println("Default Constructor");
+		System.out.println("Default Constructor from child class");
 		
 	}
 	public ChildClass(String a)
 	{
 		super(a);
-		System.out.println("Parametarized Constructor");
+		System.out.println("Parametarized Constructor from child class");
 	}
 }
--- Assignment4.4/src/ParentClass.java
+++ Assignment4.4/src/ParentClass.java
@@ -4,11 +4,11 @@ public class ParentClass {
 	protected String name;
 	public ParentClass()
 	{
-	  System.out.println("Default Constructor");
+	  System.out.println("Default Constructor from parent class");
 	}
 	public ParentClass(String a)
 	{
 		this.name=a;
-		System.out.println("Parametrized constructor");
+		System.out.println("Parametrized constructor from parent class");
 	}
 }
