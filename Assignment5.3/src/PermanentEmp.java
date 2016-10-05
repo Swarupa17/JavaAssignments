df761b989b146fa0830484d1f9b8bd214224e564
Swarupa17 <swarupa.khapli@yahoo.in>
2016-10-05 15:16:46 +0530

    Employee

---
+++ Assignment5.3/src/PermanentEmp.java
@@ -0,0 +1,28 @@
+
+public class PermanentEmp extends Employee {
+    int paid_leave,sick_leave,casual_leave;
+    double basic,hra,pfa;
+   
+	@Override
+	void calculate_balance_leaves() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	boolean avai_leave(int no_of_leaves, char type_of_leave) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	void calculate_salary() {
+		// TODO Auto-generated method stub
+		
+	}
+	void print_leave_details()
+	{
+		
+	}
+
+}
---
+++ Assignment5.3/src/TemporaryEmp.java
@@ -0,0 +1,22 @@
+
+public class TemporaryEmp extends Employee{
+
+	@Override
+	void calculate_balance_leaves() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	boolean avai_leave(int no_of_leaves, char type_of_leave) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	void calculate_salary() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
